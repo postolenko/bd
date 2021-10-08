eed28df70a0e19806f56c58502c02226b4b97ef0
postolenko <denis.postolenko@gmail.com>
2021-10-08 05:48:08 +0300

    BD

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # http://postolenko.github.io/bd/index.html
 # http://postolenko.github.io/bd/simple.html
+# http://postolenko.github.io/bd/portfolio.html
+# http://postolenko.github.io/bd/contacts.html
 # http://postolenko.github.io/bd/404.html
--- js/scripts.js
+++ js/scripts.js
@@ -93,4 +93,57 @@ $(document).ready(function() {
       });
     }
 
+
+    if( $(".project_slider").length > 0 ) {
+      $(".project_slider").not(".slick-initialized").slick({
+          dots: false,
+          arrows: true,
+          // autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"></button>',
+          nextArrow: '<button class="slick-next" aria-label="Next" type="button"></button>',
+          // responsive: [
+          //     {
+          //       breakpoint: 1100,
+          //       settings: {
+          //         slidesToShow: 2,
+          //         slidesToScroll: 1
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 900,
+          //       settings: {
+          //         slidesToShow: 4,
+          //         slidesToScroll: 1
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 768,
+          //       settings: {
+          //         slidesToShow: 3,
+          //         slidesToScroll: 1
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 510,
+          //       settings: {
+          //         slidesToShow: 2,
+          //         slidesToScroll: 1
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 400,
+          //       settings: {
+          //         slidesToShow: 1,
+          //         slidesToScroll: 1
+          //       }
+          //     }
+          //   ]
+      });
+    }
+
 });
